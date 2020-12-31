5b7c81b4ec7e01a06cb110b9d0825b60b8dbc083
yxd3549 <yxd3549@rit.edu>
2020-12-30 21:05:27 -0500

    clean up code

--- popup.js
+++ popup.js
@@ -1,22 +1,8 @@
-// let changeColor = document.getElementById('changeColor');
-
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-
-// changeColor.onclick = function(element) {
-//     let color = element.target.value;
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//         chrome.tabs.executeScript(
-//             tabs[0].id,
-//             {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//     });
-//   };
-
 let scrapeButton = document.getElementById('scrape-button');
+let resultsContainer = document.getElementById('results');
 
 scrapeButton.onclick = function(element) {
+  resultsContainer.textContent = ''; // reset in case of multiple requests
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
@@ -29,7 +15,8 @@ scrapeButton.onclick = function(element) {
   // If no results are retrieved in 3 seconds, display message
   setTimeout(function(){ 
     spinner.style.display = 'none';
-    let resultsContainer = document.getElementById('results');
-    resultsContainer.textContent = 'No results were found. Go to the Assignments tab or the Quizzes tab to retrieve deadlines';
+    if (!resultsContainer.textContent) { 
+      resultsContainer.textContent = 'No results were found. Go to the Assignments tab or the Quizzes tab to retrieve deadlines';
+    }
   }, 3000);
 }
