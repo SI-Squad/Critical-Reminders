9c26100f66aa3e359fccaa35da115a20f9815ad7
Aaron Putterman <abp6318@rit.edu>
2021-01-08 17:01:21 -0500

    Retrieving assignments now adds them to form and appends the form to the results container.

--- popup.js
+++ popup.js
@@ -15,8 +15,8 @@ scrapeButton.onclick = function(element) {
   // If no results are retrieved in 3 seconds, display message
   setTimeout(function(){ 
     spinner.style.display = 'none';
-    if (!resultsContainer.textContent) { 
-      resultsContainer.textContent = 'No results were found. Go to the Assignments tab or the Quizzes tab to retrieve deadlines';
+    if (!resultsContainer.hasChildNodes) { 
+      resultsContainer.hasChildNodes = 'No results were found. Go to the Assignments tab or the Quizzes tab to retrieve deadlines';
     }
   }, 3000);
 }
@@ -25,21 +25,38 @@ scrapeButton.onclick = function(element) {
 chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
   for(index=0; index<response[2]; index++){
     console.log(response[0][index] + " is due on " + response[1][index]);
-    var textNode = document.createTextNode(response[0][index] + " is due on " + response[1][index]);
-    resultsContainer.appendChild(textNode);
-    resultsContainer.appendChild(document.createElement("BR"));
+    // var textNode = document.createTextNode(response[0][index] + " is due on " + response[1][index]);
+    // resultsContainer.appendChild(textNode);
+    // resultsContainer.appendChild(document.createElement("BR"));
 
-    // things to check for
-    // - Missing due date
-    // - Missing assignment title
 
-    // things to add
-    // - Class title or abbreviation
+    var form = document.createElement("FORM");
 
-    // for the future...
-    // input value for title - required
-    // input value for notes
-    // date picker - required 
-    // time picker
+    var title = document.createElement("INPUT");
+    title.setAttribute("type", "text");
+    title.setAttribute("placeholder", "Title");
+    title.required;
+    title.setAttribute("value", response[0][index]);
+
+    var description = document.createElement("INPUT");
+    description.setAttribute("type", "text");
+    description.setAttribute("placeholder", "Description");
+
+    var datePicker = document.createElement("INPUT");
+    datePicker.setAttribute("type", "date");
+    datePicker.required;
+
+    var timePicker = document.createElement("INPUT");
+    timePicker.setAttribute("type", "time");
+
+    form.appendChild(title);
+    form.appendChild(description);
+    form.appendChild(datePicker);
+    form.appendChild(timePicker);
+    form.appendChild(document.createElement("HR"));
+
+    resultsContainer.appendChild(form);
+
+
   }
 });
