ecdc23f0392abaa56a376200e43f3c27966acf0d
yxd3549 <yxd3549@rit.edu>
2021-02-14 19:00:50 -0500

    Move help links to only appear after download button is clicked

--- ics-generator.js
+++ ics-generator.js
@@ -2,8 +2,6 @@ var calendar = ics();
 downloadButton = document.getElementById('download-button');
 downloadButton.onclick = function(element) {
     calendar.download();
+    const helpLinks = document.getElementById("help-links");
+    helpLinks.style.display = "block";
 }
-
-
-calendar.addEvent('Demo Event', 'This is an all day event', 'Nome, AK', '1/21/2021', '1/21/2021');
-calendar.addEvent('Demo Event', 'This is thirty minute event', 'Nome, AK', '1/21/2021 5:30 pm', '1/21/2021 6:00 pm');
